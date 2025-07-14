792413897fcf3b3039e25d6eab8e40c6ed6d9482
amruthtk <69493048+amruthtk@users.noreply.github.com>
2025-07-14 15:21:33 +0530

    Add files via upload

---
+++ PR1/pr1.java
@@ -0,0 +1,28 @@
+
+package com.example.prgrm_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.prgrm_2.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn=findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "HELLO !!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
+
